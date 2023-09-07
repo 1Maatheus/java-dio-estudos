683759ef187f0371aacbb819eeab770ab5ca07b9
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-09-07 14:27:01 -0300

    Atualização

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        
+        //Exemplo de declaração de variáveis:
+
+        int number = 10;
+        String name = "Matheus";
+        double number2 = 2.44;
+        boolean isTrue = true; 
+
+        //Declaração de constante
+
+        final double VALOR_PI = 3.14;
+
+    }
+}
