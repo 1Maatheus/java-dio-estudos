17ab3d24b0c639775f086fb8a748b9310a223719
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-09-07 14:46:13 -0300

    commit

--- tipos-operadores/src/operadores.java
+++ tipos-operadores/src/operadores.java
@@ -1,5 +1,22 @@
 public class operadores {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        //Operadores aritméticos: São os operadores matemáticos que já conhecemos e usamos no dia a dia.
+
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 90;
+        int multplicacao = 20 * 7;
+        int divisao = 14 / 2;
+        int modulo = 20 % 2;
+        double resultado = (10 * 9) + (20 / 4);
+
+        //O operador de soma pode gerar problemas caso uma operação de soma com strings aconteça, pois ele não irá realizar a soma e sim, uma concatenação dos valores atribuídos.
+
+        // ----------------------------------------------------------------------------
+
+        //Operadores unários: são operadores que são aplicados com outro operador aritmético. Eles realizam alguns trabalhos básicos como incrementar, decrementar, inverter valores numériocs e booleanos.
+
+        
+
     }
 }
