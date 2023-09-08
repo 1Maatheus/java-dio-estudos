a761e8395116fbd8df6aa92aaaa891cf8bcef484
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-09-08 14:15:53 -0300

    commit

---
+++ escopo/src/escopo.java
@@ -0,0 +1,6 @@
+public class escopo {
+    public static void main(String[] args) throws Exception {
+
+          //O escopo pode ser entendido como o ambiente onde uma variável pode ser acessada. Em java, o escopo de variáveis vai de acordo com o bloco onde ela foi declarada.
+ }
+}
