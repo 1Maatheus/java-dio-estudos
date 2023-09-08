d39ef95d147c5022a3bcbe0cacf5364e1ca4cd09
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-09-08 14:23:27 -0300

    Adicionado interação com usuário através de prompt

--- terminal/src/MinhaClasse.java
+++ terminal/src/MinhaClasse.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class MinhaClasse {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite a sua altura: ");
+        double altura = scanner.nextDouble();
+
+
+        System.out.println("Olá, me chamo " + nome + "!");
+        System.out.println("Tenho " + idade + " anos.");
+        System.out.println("Minha altura é " + altura + " cm.");
     }
 }
