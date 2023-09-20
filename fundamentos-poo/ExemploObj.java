803e2fd4ed56d0c571567a4f1488ea02b9d26da8
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-09-20 17:05:32 -0300

    MSN

--- fundamentos-poo/ExemploObj.java
+++
@@ -1,5 +0,0 @@
-public class ExemploObj {
-  public static void main(String[] args) {
-    
-  }
-}
---
+++ fundamentos-poo/MSNMessenger.java
@@ -0,0 +1,17 @@
+public class MSNMessenger {
+  public void enviarMensagem() {
+    System.out.println("Enviando mensagem...");
+  }
+
+  public void receberMensagem() {
+    System.out.println("Recebendo mensagem...");
+  }
+
+  public void validarConectadoInternet() {
+    System.out.println("Validando conexão com a rede...");
+  }
+
+  public void salvarHistoricoMensagem() {
+    System.out.println("Salvando o histórico de mensagens...");
+  }
+}
