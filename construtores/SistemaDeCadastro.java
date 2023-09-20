a61b682a11425b9ef26aef162a4977be1ef8ce0a
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-09-20 15:26:46 -0300

    Constructors

---
+++ construtores/SistemaDeCadastro.java
@@ -0,0 +1,8 @@
+package construtores;
+
+public class SistemaDeCadastro {
+  public static void main(String[] args) {
+    Pessoa person = new Pessoa("461.769.058-18", "Matheus");
+    person.setEndereco("Rua Luiz Coutinho de Abreu");
+  }
+}
